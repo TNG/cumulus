33fe801577f362dddc53996587321fe09fc4fd2f
Christoph Niehoff <christoph.niehoff@tngtech.com>
2022-11-15 10:36:44 +0100

    rudimentary version of a cover page

--- utils/build_pdf.sh
+++ utils/build_pdf.sh
@@ -4,12 +4,18 @@ function get_current_version() {
   local tag_at_head
   local version_name
   tag_at_head="$(git name-rev --name-only --tags HEAD | sed 's/^undefined$//')"
-  version_name="${tag_at_head:-snapshot-"$(git rev-parse --short HEAD)"}"
+  version_name="${tag_at_head:-snapshot/"$(git rev-parse --short HEAD)"}"
   echo "${version_name}"
 }
 
 function clean_tmp_files() {
   git clean -dfX || true
+}
+
+function write_version_file() {
+  local version="${1}"
+
+  echo "(version ${version})" > tex/version.tex
 }
 
 function compile() {
@@ -45,4 +51,5 @@ current_version="$(get_current_version)"
 echo "Building version ${current_version}"
 
 clean_tmp_files
+write_version_file "${current_version}"
 compile "cumulus" single_pages.tex
